33336adbed9bbcaf21283107537f33a06547bf30
FVTVLIX <andman290@gmail.com>
2020-05-14 10:47:45 -0400

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+    
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" />
+          <Route path="/products" />
+          <Route path="/add-product" />
+          <Route path="/products/:id/edit"  />
+          <Route path="/products/:id" />
+          <Route path="/login" />
+          <Route path="/logout" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
