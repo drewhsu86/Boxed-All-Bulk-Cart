54b8b2cc4de57c07e46fb185673dad82cf20e434
Luke Sweeney <llsween95@gmail.com>
2020-05-14 13:30:46 -0400

    added categories to sidebar nav

--- client/src/components/SideNav/SideNav.jsx
+++ client/src/components/SideNav/SideNav.jsx
@@ -3,9 +3,21 @@ import React, { Component } from 'react'
 class SideNav extends Component {
   render() {
     return (
-      <div>
-        Side Nav
-      </div>
+      <nav>
+        <h3>Categories</h3>
+        <li>All Bulk</li>
+        <li>Snacks</li>
+        <li>Beverages</li>
+        <li>Grocery</li>
+        <li>Household Essentials</li>
+        <li>Personal Care</li>
+        <li>Health and Wellness</li>
+        <li>Baby</li>
+        <li>Home</li>
+        <li>School and Office</li>
+        <li>Pets</li>
+        <li>Wine</li>
+      </nav>
     )
   }
 }
