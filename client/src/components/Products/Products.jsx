92339c477a043698bbfa4a0c123944d56ccf90ed
Luke Sweeney <llsween95@gmail.com>
2020-05-14 11:11:37 -0400

    created products dummy data

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -52,6 +52,16 @@ const productsData = [
     stock: 25,
     categories: 'grocery',
     subcategory: 'cereal & breakfast'
+  },
+  {
+    name: 'Product 006',
+    imgURL: 'https://www.nestle-cereals.com/global/sites/g/files/qirczx356/f/stage_visual/images_15.jpg',
+    description: 'Yummy in my tummy :)',
+    price: '4.99',
+    rating: 3,
+    stock: 25,
+    categories: 'grocery',
+    subcategory: 'cereal & breakfast'
   }
 ]
 
