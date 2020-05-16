e764ac2eb12ef168629de90db650838498a6eddd
drewhsu86 <bowserhsu@gmail.com>
2020-05-16 17:19:38 -0400

    got the items into 2 flex columns on productDetails page

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -3,6 +3,7 @@ import Reviews from '../ProductDetails/Reviews/Reviews'
 import Info from './Info/Info'
 import Images from './Image/Images'
 import RelatedItems from './RelatedItems/RelatedItems'
+import './ProductDetails.css'
 
 const data = require('../data.json')
 
@@ -20,12 +21,16 @@ export default class ProductDetails extends Component {
     console.log(this.state.items)
     return (
       <div className="detail-container">
-        <div className="prodColumn">
-          <Images images={this.state.items[0].images} altText={this.state.items[0].name} />
+        <div className="prodColumn left">
+          <div className="topRow">
+            <Images images={this.state.items[0].images} altText={this.state.items[0].name} />
+          </div>
           <Reviews />
         </div>
-        <div className="prodColumn">
-          <Info items={this.state.items} />
+        <div className="prodColumn right">
+          <div className="topRow">
+            <Info items={this.state.items} />
+          </div>
           <RelatedItems items={this.state.items} />
         </div>
       </div>
