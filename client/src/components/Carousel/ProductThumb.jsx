52a5c4fbe6220bf0e5e0fa5d385b54c290e24d88
drewhsu86 <bowserhsu@gmail.com>
2020-05-19 10:47:30 -0400

    updated appearance and info shown by productThumb

--- client/src/components/Carousel/ProductThumb.jsx
+++ client/src/components/Carousel/ProductThumb.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './ProductThumb.css'
+import StarRating from '../StarRating'
 
 class ProductThumb extends Component {
+
+  handleClick = (prodID) => {
+    const addToCart = this.props.addToCart || function (id) { console.log('add to cart: ' + id) }
+
+    addToCart(prodID)
+  }
+
   render() {
 
     const product = this.props.product || {
@@ -14,11 +22,18 @@ class ProductThumb extends Component {
     return (
       <div className="productThumb">
         <Link to={`/productdetails/${product['_id']}`}>
-          <h3>{product.name}</h3>
+
           {product.images[0] ? <img src={product.images[0]}
             alt="Product Image" /> : <h5>No Image Available</h5>}
+
+          <h4>{product.name}</h4>
         </Link>
+
         <p>{product.description}</p>
+        <StarRating rating={product.rating} />
+        <h3>${product.price}</h3>
+        <p>Save 12% vs retail</p>
+        <button onClick={() => { this.handleClick(product['_id']) }}>Add to cart</button>
       </div>
     )
   }
