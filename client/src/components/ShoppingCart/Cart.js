6881fd32b9e335823ed891b9f3096863ced505e2
drewhsu86 <bowserhsu@gmail.com>
2020-05-20 10:39:30 -0400

    changed css opacity from percent to number between 0 and 1

--- client/src/components/ShoppingCart/Cart.js
+++ client/src/components/ShoppingCart/Cart.js
@@ -4,13 +4,13 @@ import CartItem from './CartItem'
 export default function Cart(props) {
 
   const items = props.items
-  const total = items.reduce((accum, item) => {
+  const total = items ? items.reduce((accum, item) => {
     if (!isNaN(Number(item.product.price))) {
       return accum + (Number(item.product.price) * item.quantity)
     } else {
       return accum
     }
-  }, 0)
+  }, 0) : 0
 
   return (
     <div className="cart" style={props.addStyle}>
